63d3f347ab20bd76092c19a3abcb4397b2507f2b
yanghy <3868850350@qq.com>
2025-05-02 18:27:57 +0800

    搭建小实战和Sequential的使用

--- src/nn_seq.py
+++ src/nn_seq.py
@@ -4,6 +4,11 @@ from torch import nn
 from torch.nn import Conv2d,MaxPool2d,Flatten,Linear
 from torch.utils.tensorboard import SummaryWriter
 
+##有很多东西大致上是一样的（构建神经网络，创建实例....包括后面写入，用tensorboard）
+##Sequential和Compose本质一样，都是把好几个方法放在一块，让代码看起来更加简洁
+##这里面都是nn.xxx是因为有tensorflow这个package，会被误认
+#小技巧：1.有时候怕打错可以直接写nn.xxx让他自动补全直接tab
+#      2.如果不知道卷积/池化的参数可以print一下看看是多少然后填进去
 
 class HY(nn.Module):
     def __init__(self):
